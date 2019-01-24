34b5f581459f181a2752a15b8d7787eddae7cad4
Antoni Florit Homar <toniflorithomar@gmail.com>
2019-01-24 19:10:12 +0100

    delete twitter auth

--- setting.py
+++ setting.py
@@ -4,11 +4,4 @@ import os
 setting = {
     'BUCKET_NAME': 'you bucket name',
     'ACCESS_KEY': 'you access tocken',
-    'TWITTER_AUTH': {
-        'consumer_key': 'ABCDEFGHIJKLKMNOPQRSTUVWXYZ',
-        'consumer_secret': '1234567890ABCDEFGHIJKLMNOPQRSTUVXYZ',
-        'access_token': 'ZYXWVUTSRQPONMLKJIHFEDCBA',
-        'access_token_secret': '0987654321ZYXWVUTSRQPONMLKJIHFEDCBA',
-    },
-    'CITY': 'your city'
 }
