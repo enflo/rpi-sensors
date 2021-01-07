6a1260c95997a4e7de4f6b0c35ada3aeed6bd69b
Toni <toniflorithomar@gmail.com>
2021-01-07 16:57:59 +0100

    add influx

--- app.py
+++ app.py
@@ -5,16 +5,22 @@ import datetime
 import requests
 import json
 
+from influxdb import InfluxDBClient
+
 DHT_SENSOR = Adafruit_DHT.AM2302
 DHT_PIN = 4
 
 DEVICE = ["Raspberry Pi 3+", "DHT22"]
 USERNAME = "Antoni Florit Homar"
-URL= "https://weather.toniflorithomar.dev/ambient/"
+URL = "https://weather.toniflorithomar.dev/ambient/"
+INFLUXURL = "https://influx.toniflorithomar.dev/write?db=weather"
 CITY_NAME = "Palma"
 LONGIUDE = "2.67788529"
 LATITUDE = "39.58578071"
 SEALEVEL = "27 m"
+
+client = InfluxDBClient(host='influx.toniflorithomar.dev', ssl=True, verify_ssl=True)
+client.switch_database('weather')
 
 try:
     while True:
@@ -53,6 +59,7 @@ try:
         print(payload)
         response = requests.request("POST", URL, headers=headers, data=json.dumps(payload))
         print(response)
+        client.write_points(json.dumps(payload))
         time.sleep(300)
 
 except KeyboardInterrupt:
